a1c8d07b0d791314b595a3082f9c682d5574823d
lucasFilppe <lucasfilipe2323@icloud.com>
2022-06-22 23:42:50 -0300

    alterando botao criar sala

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,60 +1,61 @@
+//import { auth, firebase } from '../services/firebase'
 
-import {auth, firebase} from '../services/firebase';
-
-import logoImg from '../assets/logo_mob.svg';
+import logoImg from '../assets/logo_mob.svg'
 //import logoGoogle from '../assets/images/icons8-google-logo.svg ';
 
-import '../styles/auth.scss';
+import '../styles/auth.scss'
 
-import {ButtonAsk} from '../components/ButtonAsk';
-import { useHistory } from 'react-router-dom';
+import { ButtonAsk } from '../components/ButtonAsk'
+//import { useHistory } from 'react-router-dom'
 
-export function Home(){
+export function Home() {
+  //const history = useHistory()
 
-    const history = useHistory();
-    
-    //funçao que navega pelas paginas
-    function handleCreateRoom() {
+  //funçao que navega pelas paginas
+  // function handleCreateRoom() {
 
-        //parte de atenticação do usuario
-        const provider = new firebase.auth.GoogleAuthProvider();
+  //parte de atenticação do usuario
+  //const provider = new firebase.auth.GoogleAuthProvider();
+  //
 
-        auth.signInWithPopup(provider).then(result => {
-            console.log(result);
-            
-        })
+  // auth.signInWithPopup(provider).then(result => {
+  //  console.log(result);
 
-        //history.push('/rooms/new');
-    }
+  //})
 
-    return(
-        <div id="page-auth">
-            <aside> 
-                <img src={logoImg} alt="logo"/>
-               <strong>Crie Salas de Q&amp;A ao-vivo</strong>
-               <p>Tire as duvidas da sua audiência em tempo real.</p>
-            </aside>
+  //history.push('/rooms/new');
+  // }
 
-            <main>
-                <div className="main-content">
-                   <button onClick={handleCreateRoom} className="create-room">
-                       Crie sua sala com o google
-                   </button>
-                  <div className="separator">
-                      Ou entre em uma sala
-                  </div>
-                <form>
-                    <input
-                        type="text"
-                        placeholder="Digite o código da sala"/>
-                    <ButtonAsk type="submit">
-                        Entrar na sala
-                    </ButtonAsk>
-                </form>
-                </div>
-            </main>
+  //função que da um alert ao usuario para criar conta no google
+  function logar() {
+    const aviso = alert('Crie uma conta google')
+    return aviso
+
+
+  }
+
+  return (
+    <div id="page-auth">
+      <aside>
+        <img src={logoImg} alt="logo" />
+        <strong>Crie Salas de Q&amp;A ao-vivo</strong>
+        <p>Tire as duvidas da sua audiência em tempo real.</p>
+      </aside>
+
+      <main>
+        <div className="main-content">
+          <button onClick={logar} className="create-room">
+            Crie sua sala com o google
+          </button>
+          <div className="separator">Ou entre em uma sala</div>
+          <form>
+            <input type="text" placeholder="Digite o código da sala" />
+            <ButtonAsk type="submit">Entrar na sala</ButtonAsk>
+          </form>
         </div>
-    );
+      </main>
+    </div>
+  )
 }
 
 //<img src={logoImg} alt="logo do site" />
